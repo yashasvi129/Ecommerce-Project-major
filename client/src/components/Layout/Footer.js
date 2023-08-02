de6b40e2fe259c38377e3d605ae4523be97a40f6
yashasvi129 <96834169+yashasvi129@users.noreply.github.com>
2023-08-02 22:11:55 +0530

    Update Footer.js

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="footer">
-      <h1 className="text-center">All Right Reserved &copy; Techinfoyt</h1>
+      <h1 className="text-center">All Right Reserved &copy; Yashasvi</h1>
       <p className="text-center mt-100000px">
         <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
         <Link to="/policy">Privacy Policy</Link>
